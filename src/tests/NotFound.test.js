70f3b9656612e0de8a14ea41e699e63360d274c9
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-07-26 21:29:53 -0300

    Adciona tests da página NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('1. A página contém um heading h2 com o texto \'Page requested not found\':',
+    () => {
+      renderWithRouter(<NotFound />);
+      expect(screen.getByRole('heading', {
+        name: /Page requested not found/i,
+        level: 2,
+      })).toBeInTheDocument();
+    });
+
+  it('2. A página mostra uma imagem',
+    () => {
+      renderWithRouter(<NotFound />);
+      const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+      const image = screen
+        .getByAltText(/Pikachu crying because the page requested was not found/i);
+
+      expect(image).toHaveAttribute('src', src);
+    });
+});
